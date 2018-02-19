3ecf5a234cec5067985cfb842e7e479cdc666eb5
Pateriya <raghvendra.pateriya@cdk.com>
2018-02-19 23:14:02 +0530

    Set debug true

--- recruitment_site/settings.py
+++ recruitment_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '!^_c3gofr(0=2%3!=##h%p(!n)yb4gljm3c6f7o2hz4rv7@q(*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
